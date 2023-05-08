833f0c9232c8432effc617d2a386e6a9554337ac
Anton Baumann <anton@antonbaumann.com>
2023-05-08 18:18:50 +0200

    collate stuff

--- tasks/nyuv2_depth/datamodule.py
+++ tasks/nyuv2_depth/datamodule.py
@@ -3,10 +3,26 @@ from argparse import ArgumentParser, Namespace
 import pytorch_lightning as pl
 import numpy as np
 import torch
+from torch.utils.data import Dataset
 import h5py
 
 from utils import dir_path
 
+
+class NYUv2DepthDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, index):
+        image = self.data['image'][index]
+        label = self.data['label'][index]
+        return {
+            'image': torch.tensor(image).permute(2, 0, 1).float(),
+            'label': torch.tensor(label).unsqueeze(0).float(),
+        }
+
+    def __len__(self):
+        return len(self.data['image'])
 
 class NYUv2DepthDataModule(pl.LightningDataModule):
     def __init__(
@@ -22,12 +38,6 @@ class NYUv2DepthDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
         self.pin_memory = pin_memory
 
-    def collate_fn(self, batch):
-        images = torch.stack([torch.tensor(sample['image']).permute(2, 0, 1).float() for sample in batch])
-        depths = torch.stack([torch.tensor(sample['label']).unsqueeze(0).float() for sample in batch])
-
-        return {'image': images, 'label': depths}
-
     def setup(
         self,
         stage: Optional[str] = None,
@@ -35,15 +45,15 @@ class NYUv2DepthDataModule(pl.LightningDataModule):
         h5_train = h5py.File("/ws/data/nyuv2/depth_train.h5", "r")
         h5_test = h5py.File("/ws/data/nyuv2/depth_test.h5", "r")
 
-        self.data_train = dict(
+        self.data_train = NYUv2DepthDataset(dict(
             image=h5_train["image"],
             label=h5_train["depth"],
-        )
+        ))
 
-        self.data_test = dict(
+        self.data_test = NYUv2DepthDataset(dict(
             image=h5_test["image"],
             label=h5_test["depth"],
-        )
+        ))
 
     def train_dataloader(self) -> torch.utils.data.DataLoader:
         return torch.utils.data.DataLoader(
@@ -53,7 +63,6 @@ class NYUv2DepthDataModule(pl.LightningDataModule):
             shuffle=True,
             drop_last=True,
             pin_memory=self.pin_memory,
-            collate_fn=self.collate_fn,
         )
     
     def val_dataloader(self) -> torch.utils.data.DataLoader:
@@ -64,7 +73,6 @@ class NYUv2DepthDataModule(pl.LightningDataModule):
             shuffle=False,
             drop_last=False,
             pin_memory=self.pin_memory,
-            collate_fn=self.collate_fn,
         )
     
     def test_dataloader(self) -> torch.utils.data.DataLoader:
@@ -75,7 +83,6 @@ class NYUv2DepthDataModule(pl.LightningDataModule):
             shuffle=False,
             drop_last=False,
             pin_memory=self.pin_memory,
-            collate_fn=self.collate_fn,
         )
 
 
