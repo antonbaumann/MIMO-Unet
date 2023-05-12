637537b3782a5f7bdfb7e55a76f57dd6b812130e
Anton Baumann <anton@antonbaumann.com>
2023-05-12 14:47:09 +0200

    add muad depth training

--- datasets/muad.py
+++ datasets/muad.py
@@ -5,6 +5,7 @@ import imageio.v3 as iio
 from PIL import Image
 import cv2
 from torch.utils.data import Dataset
+from scipy import interpolate
 import os
 from tqdm import tqdm
 
@@ -34,6 +35,22 @@ def resize_img(
         interpolation=cv2.INTER_NEAREST,
     )
     return data
+
+def fix_depth_map(img):
+    # Get the x, y coordinates of pixels that are not NaN
+    known_points = np.array(np.where(~np.isnan(img))).T
+    known_values = img[~np.isnan(img)]
+
+    # Get the x, y coordinates of all pixels in the image
+    all_points = np.array(np.where(np.ones_like(img))).T
+
+    # Use scipy's griddata to interpolate at the missing points
+    img_fixed = interpolate.griddata(known_points, known_values, all_points, method='nearest')
+    
+    # Reshape the interpolated data to the shape of the original image
+    img_fixed = img_fixed.reshape(img.shape)
+
+    return img_fixed
 
 def get_filename_id(file_name: str) -> int:
     return int(file_name.split('_')[0])
@@ -83,6 +100,10 @@ class MUADBaseDataset(Dataset):
         if self.dsize is not None:
             image = resize_img(image, dsize=self.dsize)
             label = resize_img(label, dsize=self.dsize)
+
+        # fill missing pixels in depth map
+        if label.dtype == np.float32:
+            label = fix_depth_map(label)
 
         if self.normalize:
             image = image / 255.0
