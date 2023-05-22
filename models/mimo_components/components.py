8c7382ac5f43273ed9939fda5987a046c13396f5
Anton Baumann <anton@antonbaumann.com>
2023-05-22 18:46:44 +0200

    add plateau lr digs

--- models/mimo_components/components.py
+++ models/mimo_components/components.py
@@ -23,11 +23,6 @@ class DoubleConv(nn.Module):
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, padding_mode="reflect", groups=groups),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-
-            # nn.Conv2d(mid_channels, mid_channels, kernel_size=3, padding=1, padding_mode="reflect", groups=groups),
-            # nn.BatchNorm2d(mid_channels),
-            # nn.ReLU(inplace=True),
-
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, padding_mode="reflect", groups=groups),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
--- models/mimo_unet.py
+++ models/mimo_unet.py
@@ -260,7 +260,7 @@ class MimoUnetModel(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=12, gamma=0.5, verbose=True)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, verbose=True)
         return [optimizer], [scheduler]
     
     @staticmethod
