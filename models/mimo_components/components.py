1f1cddbb21bba4804b2fa0ccf36706ec2a72bbec
Anton Baumann <anton@antonbaumann.com>
2023-06-13 16:39:13 +0200

    no batchnorm

--- models/mimo_components/components.py
+++ models/mimo_components/components.py
@@ -21,10 +21,10 @@ class DoubleConv(nn.Module):
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, padding_mode="reflect", groups=groups),
-            nn.BatchNorm2d(mid_channels),
+            # nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, padding_mode="reflect", groups=groups),
-            nn.BatchNorm2d(out_channels),
+            # nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Dropout2d(dropout_rate),
         )
