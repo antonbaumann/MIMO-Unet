53e15ff98b51466e5c3d72a20af495124e1d9100
Anton Baumann <anton@antonbaumann.com>
2023-06-22 09:54:35 +0200

    add ndvi test script

--- scripts/test/test_ndvi.py
+++ scripts/test/test_ndvi.py
@@ -24,7 +24,7 @@ def fgsm_attack(image, epsilon, data_grad):
     # Return the perturbed image
     return perturbed_image
 
-def make_predictions(model, dataset, device: str, batch_size: int = 5, num_workers: int = 30):
+def make_predictions(model, dataset, device: str, batch_size: int = 5, epsilon: float = 0.0, num_workers=30):
     inputs = []
     y_preds = []
     y_trues = []
@@ -40,13 +40,38 @@ def make_predictions(model, dataset, device: str, batch_size: int = 5, num_worke
 
     for data in tqdm(loader):
         images = data['image'].to(device)
+        labels = data['label'].cpu()
+
+        labels = labels.unsqueeze(1)
+        labels = labels.repeat(1, model.num_subnetworks, 1, 1, 1)
+
+        images.requires_grad = True
+        labels.requires_grad = True
+
         y_pred, log_param = model(images)
+
+        loss = model.loss_fn(y_pred, log_param, labels)
+
+        # Zero all existing gradients
+        model.zero_grad()
+
+        # Calculate gradients of model in backward pass
+        loss.backward()
+
+        # Collect datagrad
+        data_grad = images.grad.data
+
+        # Call FGSM Attack
+        perturbed_data = fgsm_attack(images, epsilon, data_grad)
+
+        # Predict on the perturbed image
+        y_pred, log_param = model(perturbed_data)
 
         y_pred = y_pred.cpu().detach()
         log_param = log_param.cpu().detach()
         y_true = data['label'].cpu().detach()
 
-        inputs.append(images.cpu().detach())
+        inputs.append(perturbed_data.cpu().detach())
         y_preds.append(y_pred)
         y_trues.append(y_true)
         log_params.append(log_param)
