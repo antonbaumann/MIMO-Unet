10a5e864dd29b2d207eef6ad46be0ac9a8fe9ab7
Anton Baumann <anton@antonbaumann.com>
2023-06-28 18:06:53 +0200

    directly reduce ensemble predictions

--- scripts/test/test_ndvi.py
+++ scripts/test/test_ndvi.py
@@ -47,8 +47,6 @@ def make_predictions(model, dataset, device: str, batch_size: int = 5, num_worke
         aleatoric_var = aleatoric_var.cpu().detach()
         epistemic_var = epistemic_var.cpu().detach()
 
-        print(y_pred.shape, aleatoric_var.shape, epistemic_var.shape, data['label'].shape)
-
         y_true = data['label'].cpu().detach()
 
         inputs.append(images.cpu().detach())
@@ -62,6 +60,12 @@ def make_predictions(model, dataset, device: str, batch_size: int = 5, num_worke
     y_trues = torch.cat(y_trues, dim=0).clip(min=0, max=1)
     aleatoric_vars = torch.cat(aleatoric_vars, dim=0)
     epistemic_vars = torch.cat(epistemic_vars, dim=0)
+
+    print(f"inputs.shape: {inputs.shape}")
+    print(f"y_preds.shape: {y_preds.shape}")
+    print(f"y_trues.shape: {y_trues.shape}")
+    print(f"aleatoric_vars.shape: {aleatoric_vars.shape}")
+    print(f"epistemic_vars.shape: {epistemic_vars.shape}")
     
     return (
         inputs,
