9dd5541af0649b0cb94ed34545dbeea59ae08ade
Anton Baumann <anton@antonbaumann.com>
2023-07-05 13:24:57 +0200

    reshape

--- models/evidential_unet.py
+++ models/evidential_unet.py
@@ -82,9 +82,9 @@ class EvidentialUnetModel(pl.LightningModule):
 
         mu, logv, logalpha, logbeta = torch.unbind(out, axis=1)
 
-        v = torch.log1p(logv) + 1e-5
-        alpha = torch.log1p(logalpha) + 1 + 1e-5
-        beta = torch.log1p(logbeta) + 1e-5
+        v = torch.log1p(logv) + 1e-4
+        alpha = torch.log1p(logalpha) + 1 + 1e-4
+        beta = torch.log1p(logbeta) + 1e-4
 
         return torch.stack([mu, v, alpha, beta], dim=1)
     
